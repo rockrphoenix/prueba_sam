330ef0232978657e07df5224df9b7e31e783ab69
julio marrufo <julio.marrufo@claroshop.com>
2025-03-19 19:45:22 -0600

    se agrega .github 2

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world from github",
+            "message": "hello world from github ahhhhh",
             # "location": ip.text.replace("\n", "")
         }),
     }
